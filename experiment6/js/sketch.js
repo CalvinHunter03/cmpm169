8f9d04d4a526b511243c9b966fac99c6a4540975
Calvin Hunter <calhunte@ucsc.edu>
2025-02-23 12:21:55 -0800

    added experiment 6
    its not completed at all

---
+++ experiment6/js/sketch.js
@@ -0,0 +1,67 @@
+// Globals
+let myInstance;
+let canvasContainer;
+var centerHorz, centerVert;
+
+let input, button, userText;
+
+class MyClass {
+    constructor(param1, param2) {
+        this.property1 = param1;
+        this.property2 = param2;
+    }
+
+    myMethod() {
+        // code to run when method is called
+    }
+}
+
+
+function resizeScreen() {
+  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
+  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
+  console.log("Resizing...");
+  resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  // redrawCanvas(); // Redraw everything based on new size
+}
+
+// setup() function is called once when the program starts
+function setup() {
+  // place our canvas, making it fit our container
+  canvasContainer = $("#canvas-container");
+
+  canvas.parent("canvas-container");
+  // resize canvas is the page is resized
+
+  // create an instance of the class
+  myInstance = new MyClass("VALUE1", "VALUE2");
+
+  $(window).resize(function() {
+    resizeScreen();
+  });
+  resizeScreen();
+
+
+
+
+
+  };
+  function draw() {
+    background(220);
+  }
+ 
+
+  function mousePressed() {
+    drawTextCircle(mouseX, mouseY, radius, texts);
+  }
+  
+  function drawTextCircle(x, y, r, txtArray) {
+    let angleStep = TWO_PI / txtArray.length;
+    for (let i = 0; i < txtArray.length; i++) {
+      let angle = i * angleStep;
+      let tx = x + cos(angle) * r;
+      let ty = y + sin(angle) * r;
+      text(txtArray[i], tx, ty);
+    }
+  }
+  
