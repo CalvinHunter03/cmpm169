eab5baac3651255cc57f7a1672a608a5b95316eb
Calvin Hunter <70008107+CalvinHunter03@users.noreply.github.com>
2025-02-27 03:16:40 -0800

    added a lot
    of things happened here

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,9 +1,36 @@
-// Globals
+const story = {
+  "start": "#[name:#person#][animal:#theAnimal#][importantItem:#item#][performAction:#action#]story#",
+  "story": "Part One: Once upon a time, #name# was walking towards the #building# when suddenly there was a noise. #name# decided to check it out and noticed a small #animal#. \"You're not looking too well\", says the #animal#. #name# hadn't had a chance to look himself in the mirror that day, for they were rushed out of their house. Waking up #number# after their alarm, with only #number# minutes to get ready. #morningActivity# and #morningActivity# takes up a lot of time. Once everything was finished, #name# forgot to grab their #importantItem# while they were leaving. Without the #importantItem#, #name# won't be able to #performAction#. We all pray that #name# is able to #performAction# because the #animal# was looking forward to watching them do it. ",
+  "person": ["Joe", "Bob", "Gorilla Man"],
+  "building": ["office", "bagel store", "climbing gym"],
+  "theAnimal": ["cat", "tiger", "walrus"],
+  "number": ["1", "2", "10", "20", "15", "50"],
+  "morningActivity": ["brushing teeth", "brushing hair", "eating breakfast", "doing 300 push ups", "drawing eyebrows on", "10 minutes of praying to God", "drinking water", "putting shoes on"],
+  "item": ["notebook", "wallet", "phone", "keys", "teddy bear", "support blanket", "sunglasses", "keyboard", "ball and chain", "gauntlet", "really heavy hammer", "pc setup"],
+  "action": ["save the princess", "jump on the bed", "play minecraft", "listen to music", "get down on all fours and crawl", "hold a normal conversation without thinking about her", "dance"]
+}
+
+const myCreation = {
+  "start": "#[name:#person#][animal:#theAnimal#][importantItem:#item#][performAction:#action#]story#",
+  "story": "Part One: Once upon a time, #name# was walking towards the #building# when suddenly there was a noise. #name# decided to check it out and noticed a small #animal#. \"You're not looking too well\", says the #animal#. #name# hadn't had a chance to look himself in the mirror that day, for they were rushed out of their house. Waking up #number# after their alarm, with only #number# minutes to get ready. #morningActivity# and #morningActivity# takes up a lot of time. Once everything was finished, #name# forgot to grab their #importantItem# while they were leaving. Without the #importantItem#, #name# won't be able to #performAction#. We all pray that #name# is able to #performAction# because the #animal# was looking forward to watching them do it. ",
+  "person": ["Joe", "Bob", "Gorilla Man"],
+  "building": ["office", "bagel store", "climbing gym"],
+  "theAnimal": ["cat", "tiger", "walrus"],
+  "number": ["1", "2", "10", "20", "15", "50"],
+  "morningActivity": ["brushing teeth", "brushing hair", "eating breakfast", "doing 300 push ups", "drawing eyebrows on", "10 minutes of praying to God", "drinking water", "putting shoes on"],
+  "item": ["notebook", "wallet", "phone", "keys", "teddy bear", "support blanket", "sunglasses", "keyboard", "ball and chain", "gauntlet", "really heavy hammer", "pc setup"],
+  "action": ["save the princess", "jump on the bed", "play minecraft", "listen to music", "get down on all fours and crawl", "hold a normal conversation without thinking about her", "dance"]
+}
+
+let resetButton = document.createElement("button");
+
+
+let grammer;
+let result;
+
 let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
-
-let input, button, userText;
 
 class MyClass {
     constructor(param1, param2) {
@@ -15,7 +42,6 @@ class MyClass {
         // code to run when method is called
     }
 }
-
 
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
@@ -29,8 +55,9 @@ function resizeScreen() {
 function setup() {
   // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
-
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
+  canvas.document.querySelector('.scrolling-text');
   // resize canvas is the page is resized
 
   // create an instance of the class
@@ -42,26 +69,37 @@ function setup() {
   resizeScreen();
 
 
+  grammer = tracery.createGrammar(story);
+
+  result = grammer.flatten("#start#");
+  // console.log(result);
 
 
+  //Reset Button - Create, mouse pressed
+  let resetButton = createButton('Refresh');
+  resetButton.position(canvasContainer.width() / 3, canvasContainer.height()/4);
+  resetButton.mousePressed(resetStory);
+
+  //Input button
+  let inputButton = createButton("Input");
+  inputButton.position(canvasContainer.width() * 3 / 4, canvasContainer.height() /4)
 
   };
+
+  function resetStory(){
+    window.location.reload();
+  }
+
   function draw() {
     background(220);
+
+    
+
+    rect(canvasContainer.width() / 10, canvasContainer.height() / 15, canvasContainer.width() * 4/5 , canvasContainer.height() * 13/15);
+    text(result, canvasContainer.width() / 4, canvasContainer.height() / 4, canvasContainer.width() / 2, canvasContainer.height() / 2);
   }
  
 
-  function mousePressed() {
-    drawTextCircle(mouseX, mouseY, radius, texts);
-  }
   
-  function drawTextCircle(x, y, r, txtArray) {
-    let angleStep = TWO_PI / txtArray.length;
-    for (let i = 0; i < txtArray.length; i++) {
-      let angle = i * angleStep;
-      let tx = x + cos(angle) * r;
-      let ty = y + sin(angle) * r;
-      text(txtArray[i], tx, ty);
-    }
-  }
+  
   
