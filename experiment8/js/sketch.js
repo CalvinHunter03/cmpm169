5551772eed822ac261190cf6b508c6069ee522f3
Calvin Hunter <70008107+CalvinHunter03@users.noreply.github.com>
2025-03-03 21:13:21 -0800

    added experiment 7 and 8 into the repo

---
+++ experiment8/js/sketch.js
@@ -0,0 +1,45 @@
+
+// Globals
+let myInstance;
+let canvasContainer;
+var centerHorz, centerVert;
+
+class MyClass {
+    constructor(param1, param2) {
+        this.property1 = param1;
+        this.property2 = param2;
+    }
+
+    myMethod() {
+        // code to run when method is called
+    }
+}
+
+function resizeScreen() {
+  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
+  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
+  console.log("Resizing...");
+  resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  // redrawCanvas(); // Redraw everything based on new size
+}
+
+function setup() {
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  canvas.parent("canvas-container");
+  myInstance = new MyClass("VALUE1", "VALUE2");
+
+  $(window).resize(function() {
+    resizeScreen();
+  });
+  resizeScreen();
+
+  //SETUP
+
+
+}
+
+function draw() {
+  
+}
+
