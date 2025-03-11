7b0738f8e851e0ef3f3b2733c911b17f5543b1bf
Calvin Hunter <calhunte@ucsc.edu>
2025-03-10 17:31:59 -0700

    can now search things
    You have to be super specific in spelling and what not, but it works yay

--- experiment7/js/sketch.js
+++ experiment7/js/sketch.js
@@ -53,16 +53,32 @@ function convertToListElement(listElement){
   tempListElement.innerHTML = listElement;
 
   return tempListElement;
+}
 
+function compareTheseTWo(jsonIngredientList, userSearchList){
 
+  let num = 0;
+  for(let ingredient of jsonIngredientList){
+    for(let userIngredient of userSearchList){
+      if(ingredient === userIngredient){
+        num++;
+      }
+    }
+  }
+  if(num === userSearchList.length){
+    return true;
+  }
+  return false;
 }
 
 let ingredientSet = new Set();
+
+let recipeNameSet = new Set();
 let searchList = [];
-let matchedIngredient = [];
+let recipeNameList = [];
 
 let searchListDisplay = document.createElement("div");
-
+let rightDiv = document.getElementById("right-div");
 
 let ingredientInput = document.createElement("input");
 ingredientInput.innerHTML = "Enter One Ingredient";
@@ -90,19 +106,53 @@ clearListButton.addEventListener("click", () => {
 let searchButton = document.createElement("button");
 searchButton.innerHTML = "Search";
 searchButton.addEventListener("click", () => {
-  
+  fetch('./Recipe/recipes_images.json')
+    .then(response => response.json())
+    .then(json => {
+      for(let i = 0; i < json.recipes.length; i++){
+        if(json.recipes[i].tags && json.recipes[i].tags.ingredient){          
+          let num = 0;
+          for(let ingredient of json.recipes[i].tags.ingredient){
+            for(let userIngredient of searchList){
+              if(ingredient === userIngredient){
+                num++;
+              }
+            }
+          }
+          if(num === searchList.length && json.recipes[i].title){
+            recipeNameSet.add(json.recipes[i].title);
+          }
+      } 
+    }
+    console.log(recipeNameSet)
 })
+    .catch(error => console.error('Fetch error inside search button', error))
+})
+
+/*
+https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+const str = "Crème Brûlée"
+str.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+*/
+
+
+
+
 
 let ingredientText = document.createElement("h1");
 ingredientText.innerHTML = "Enter One Ingredient";
 
 let theCanvas = document.getElementById("canvas-container");
 
-let appendList = [ingredientText, ingredientInput, addToSearchButton, clearListButton, searchListDisplay];
+let appendList = [ingredientText, ingredientInput, addToSearchButton, clearListButton, searchListDisplay, searchButton];
 
-for(let i = 0; i < appendList.length; i++){
-  theCanvas.append(appendList[i]);
-} 
+for(let element of appendList){
+  theCanvas.append(element);
+}
+
+// for(let i = 0; i < appendList.length; i++){
+//   theCanvas.append(appendList[i]);
+// } 
 
 
 
@@ -111,15 +161,14 @@ fetch('./Recipe/recipes_images.json')
   .then(response => response.json()) // Convert to JSON
   .then(json => {
     for(let i = 0; i < json.recipes.length; i++){
-      if(json.recipes[i].tags && json.recipes[i].tags.ingredient){
-        for(let j = 0; j < json.recipes[i].tags.ingredient.length; j++){
+      if(json.recipes[i].tags && json.recipes[i].tags.simple_cooking){
+        for(let j = 0; j < json.recipes[i].tags.simple_cooking.length; j++){
 
 
-          ingredientSet.add(json.recipes[i].tags.ingredient[j]);
+          ingredientSet.add(json.recipes[i].tags.simple_cooking[j]);
         }
       }
     }
-
 
     const sortedArray = Array.from(ingredientSet).sort();
     console.log(sortedArray);
